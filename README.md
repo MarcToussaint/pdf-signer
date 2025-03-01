ab568c31c9c150202d70345b0e5938ffd0e0f262
Marc Toussaint <toussaint@tu-berlin.de>
2025-03-01 18:44:14 +0100

    with args, and README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pdf signer
 
+The python script allows to place your signature (with or without date next to it) on pdfs, at the position you point with your mouse. Also a check mark and small text can be placed. It's made to quickly go through many pdfs (by default looping through all in the folder).
+
+It uses pymupdf to open and edit the pdfs, and matplotlib to display the pages pixmaps and enable crude GUI callbacks.
+
+Clicks:
+* left: place signature & date
+* shift-left: place only signature
+* middle: place a 'X' check mark
+
+Keys:
+* ' ' proceed to the next page
+* 'q' done with the document (saves it if edited)
+* 't' add the text given in the top right textbox at the location
+
+Parameters:
+* signatureFile: your signature image file
+* saveTag: the tag that is added to the filename when saving
+
+Dependencies:
+
+      pip install pymupdf numpy matplotlib
--- signer.py
+++ signer.py
@@ -2,105 +2,148 @@
 
 import os
 import pymupdf
-import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 import glob
 import subprocess
+import argparse
 
-cmd = ''
-sign_coords = []
-tick_coords = []
+import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox
+from matplotlib.backend_bases import MouseEvent
 
-def onclick(event):
-    global cmd
-    global sign_coords
-    global tick_coords
-    if event.button==plt.MouseButton.LEFT:
-        cmd='signDate'
-        sign_coords=[event.xdata, event.ydata]
-        print('signing with date', event.button, sign_coords)
-    if event.button==plt.MouseButton.RIGHT:
-        cmd='sign'
-        sign_coords=[event.xdata, event.ydata]
-        print('signing without date', event.button, sign_coords)
-    elif event.button==plt.MouseButton.MIDDLE:
-        tick_coords=[event.xdata, event.ydata]
-        print('ticking', event.button, tick_coords)
+parser = argparse.ArgumentParser(
+    description='Python script to quickly put your signature (and date, check mark, or text) on pdfs at the mouse pointer.')
 
-def on_press(event):
-    print('press', event.key)
-    global cmd
-    if event.key == 'd':
-        cmd='signDate'
-        sign_coords=[event.xdata, event.ydata]
-        print('signing with date', sign_coords)
-        plt.close(event.canvas.figure)
-    if event.key == 'd':
-        cmd='skip'
-        print('skipping')
-        plt.close(event.canvas.figure)
-        
-def sign_file(file):
-    w, h, o = 110, 30, 60
-    signature = '/home/mtoussai/lis/lis-admin/signatureColorTransparent.png'
-    date = datetime.datetime.now().strftime("%d.%m.%Y")
-    edited = False
+parser.add_argument('file', type=str, nargs='?', default='*',
+                     help='file to sign - or * to loop through all files')
 
-    if True:
-        subprocess.run(["gs","-sDEVICE=pdfwrite","-dNOPAUSE","-dQUIET","-dBATCH","-sOutputFile=z.pdf",file])
-        doc = pymupdf.open('z.pdf')
-    else:
-        doc = pymupdf.open(file)
+parser.add_argument('-saveTag', type=str, default='_signedMT', help='tag appended to the filename when saving the signed pdf')
 
-    for page in doc:
-        pix = page.get_pixmap()
-        img = np.frombuffer(pix.samples, dtype='B').reshape((pix.height,pix.width,pix.n))
+parser.add_argument('-signatureFile', type=str, default='/home/mtoussai/lis/lis-admin/signatureColorTransparent.png', help='image file of the signature')
 
-        global cmd
-        global sign_coords
-        global tick_coords
-        cmd = ''
-        sign_coords = []
-        tick_coords = []
+class Signer:
+
+    def __init__(self, args):
+        self.args = args
+        self.w, self.h, self.o = 110, 30, 60
+        self.date = datetime.datetime.now().strftime("%d.%m.%Y")
+        self.text = self.date
+        return
+
+    def sign_file(self, file):
+
+        # re-encode the pdf using ghostscript
+        if True:
+            subprocess.run(["gs","-sDEVICE=pdfwrite","-dNOPAUSE","-dQUIET","-dBATCH","-sOutputFile=z.pdf",file])
+            doc = pymupdf.open('z.pdf')
+        else:
+            doc = pymupdf.open(file)
+
+        plt.ion()
         fig = plt.figure(figsize=(10,14))
         ax = fig.add_subplot(111)
         fig.tight_layout()
-        imgplot = ax.imshow(img)
-        fig.canvas.mpl_connect('button_press_event', onclick)
-        fig.canvas.mpl_connect('key_press_event', on_press)
-        plt.show()
+        fig.canvas.mpl_connect('button_press_event', self.on_click)
+        fig.canvas.mpl_connect('key_press_event', self.on_press)
+
+        # a textbox user input to allow inserting text with 't' key
+        axbox = fig.add_axes([0.9, 0.95, 0.1, 0.05])
+        text_box = TextBox(axbox, "text", initial=self.text, textalignment="center")
+        text_box.on_submit(self.on_text)
+
+        imgplot = None
+        self.saveNewFile = False
+
+        for self.page in doc:
+            pix = self.page.get_pixmap()
+            img = np.frombuffer(pix.samples, dtype='B').reshape((pix.height,pix.width,pix.n))
+            if not imgplot:
+                imgplot = ax.imshow(img)
+                plt.show()
+            else:
+                imgplot.set_data(img)
+
+            self.pageDone = False
+            self.needsDisplayUpdate = False
+
+            while not self.pageDone: # the events below terminate this event loop
+                plt.pause(.02) # calls the matplotlib event loop - necessary to process user input
+                if self.needsDisplayUpdate: # update the pixmap
+                    pix = self.page.get_pixmap()
+                    img = np.frombuffer(pix.samples, dtype='B').reshape((pix.height,pix.width,pix.n))
+                    imgplot.set_data(img)
+                    self.needsDisplayUpdate = False
+
+            if self.cmd=='quit': # key ' ' steps through pages, key 'q' quits document
+                break
+
         fig.clear()
 
-        if len(sign_coords):
-            print('command:', cmd, sign_coords, tick_coords)
-            if cmd=='signDate':
-                rect = pymupdf.Rect(sign_coords[0]+o, sign_coords[1]-h, sign_coords[0]+o+w, sign_coords[1])
-                page.insert_image(rect, filename=signature)
-                page.insert_text((sign_coords[0], sign_coords[1]), date, fontsize=11, fontname='helv')
-            elif cmd=='sign':
-                rect = pymupdf.Rect(sign_coords[0], sign_coords[1]-h, sign_coords[0]+w, sign_coords[1])
-                page.insert_image(rect, filename=signature)
-            edited = True
+        if self.saveNewFile:
+            path, ext = os.path.splitext(file)
+            filename = f'{path}{self.args.saveTag}{ext}'
+            print('=== saving', filename)
+            doc.save(filename)
 
-        if len(tick_coords):
-            print('adding tick at', tick_coords)
-            page.insert_text((tick_coords[0]-5, tick_coords[1]+10), 'X', fontsize=18, fontname='helv')
-            edited = True
 
-        if edited:
-            break
+    def on_text(self, text):
+        self.text = text
 
-    if edited:
-        output_filename = "{}_signedMT{}".format(*os.path.splitext(file))
-        doc.save(output_filename)
+    def on_click(self, event: MouseEvent):
+        coords=[event.xdata, event.ydata]
+        print('click event ', event.button, coords, event.modifiers)
+
+        if not coords[0]:
+            print('off page')
+            return
+
+        if event.button==plt.MouseButton.LEFT: # signature
+            coords[1] += 2
+            rect = pymupdf.Rect(coords[0], coords[1]-self.h, coords[0]+self.w, coords[1])
+            self.page.insert_image(rect, filename=self.args.signatureFile)
+            if 'shift' not in event.modifiers: # and date
+                #rect = pymupdf.Rect(coords[0]+self.o, coords[1]-self.h, coords[0]+self.o+self.w, coords[1])
+                self.page.insert_text((coords[0]-self.o, coords[1]-2), self.date, fontsize=11, fontname='helv')
+
+        elif event.button==plt.MouseButton.MIDDLE: # check mark
+            self.page.insert_text((coords[0]-5.5, coords[1]+7), 'X', fontsize=16, fontname='helv')
+
+        self.needsDisplayUpdate = True
+        self.saveNewFile = True
+
+    def on_press(self, event):
+        coords=[event.xdata, event.ydata]
+        print('key event ', event.key, coords)
+
+        if event.key == ' ': # sign on more pages
+            self.pageDone = True
+
+        if event.key == 't':
+            self.page.insert_text((coords[0], coords[1]), self.text, fontsize=11, fontname='helv')
+            self.needsDisplayUpdate = True
+
+        if event.key == 'q':
+            self.cmd='quit'
+            self.pageDone = True
+
+
 
 def main():
-    files = sorted(glob.glob('*.pdf'))
+    args = parser.parse_args()
+
+    if args.file=='*':
+        files = sorted(glob.glob('*.pdf'))
+    else:
+        files = [args.file]
+
     for file in files:
-        if 'signedMT' in file or 'z.pdf' in file:
+        if args.saveTag in file or 'z.pdf' in file:
+            print('=== skipping', file)
             continue
-        sign_file(file)
+        print('=== opening', file)
+        S = Signer(args)
+        S.sign_file(file)
 
 if __name__ == "__main__":
     main()
